296f3d63575053dbf5b2e65287311b2a82c48e4d
Deeterd <deeterd@pm.me>
2020-02-03 01:55:40 +0100

    Change genesis timestamp

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -71,7 +71,7 @@ namespace CryptoNote
         /* This is the unix timestamp of the first "mined" block (technically block 2, not the genesis block)
            You can get this value by doing "print_block 2" in cirquityd. It is used to know what timestamp
            to import from when the block height cannot be found in the node or the node is offline. */
-        const uint64_t GENESIS_BLOCK_TIMESTAMP = 1512800692;
+        const uint64_t GENESIS_BLOCK_TIMESTAMP = 1579553288;
 
         const size_t CRYPTONOTE_REWARD_BLOCKS_WINDOW = 100;
 
