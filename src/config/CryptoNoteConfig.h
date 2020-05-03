b378a45522e191e4f175eb702df3f585b25e4556
Deeterd <deeterd@pm.me>
2020-05-03 23:39:23 +0200

    Activation of FUSION_DUST_THRESHOLD_HEIGHT_V2 on height 300000

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -141,7 +141,7 @@ namespace CryptoNote
 
         const uint32_t DUST_THRESHOLD_V2_HEIGHT = MIXIN_LIMITS_V2_HEIGHT;
 
-        const uint32_t FUSION_DUST_THRESHOLD_HEIGHT_V2 = 800000;
+        const uint32_t FUSION_DUST_THRESHOLD_HEIGHT_V2 = 300000;
 
         const uint64_t EXPECTED_NUMBER_OF_BLOCKS_PER_DAY = 24 * 60 * 60 / DIFFICULTY_TARGET;
 
